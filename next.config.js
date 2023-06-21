cc3dd6451d18dd409514f7045985d62f8b762a3d
juvelineorb4 <ljaranguren@gmail.com>
2023-06-21 19:27:34 -0400

    a ver que pasa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-      unoptimized: true,
-      domains: ['portaty-storage165121-dev.s3.us-east-1.amazonaws.com'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'portaty-storage165121-dev.s3.us-east-1.amazonaws.com',
-          port: '',
-          pathname: '/public/**',
-        },
-      ],
-    },
   }
   
   module.exports = nextConfig
