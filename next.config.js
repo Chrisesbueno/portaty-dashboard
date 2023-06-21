a88000bd24c268055179ee46bb327f2062c8ecfa
juvelineorb4 <ljaranguren@gmail.com>
2023-06-21 18:06:00 -0400

    probando

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'portaty-storage165121-dev.s3.us-east-1.amazonaws.com',
-          port: '',
-          pathname: '/public/app/images/brands/**',
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3bybus220215-dev.s3.amazonaws.com',
+        port: '',
+        pathname: '/public/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
   
