4361e53a1e11a78b8db64217061edfee817e361f
juvelineorb4 <ljaranguren@gmail.com>
2023-06-21 17:37:23 -0400

    porfavor funciona

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'portaty-storage165121-dev.s3.us-east-1.amazonaws.com',
-        port: '',
-        pathname: '/public/**',
-      },
-    ],
+    disableStaticImages: true
   },
 }
 
